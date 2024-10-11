61fff20d5ad64296cd9130583f0702816a487dd0
June Simmons <junecsimmons@gmail.com>
2024-10-11 18:03:14 -0500

    Still testing

--- calc.py
+++ calc.py
@@ -17,12 +17,12 @@ class Recipe:
         pass
 
 
-with open('recipes.json', 'r') as jsonfile:
+"""with open('recipes.json', 'r') as jsonfile:
     for recipe in json.load(jsonfile):
-        print(recipe)
+        print(recipe)"""
 
 
-with dpg.window(label="Recipe Calculator", width=300, height=300):
+with dpg.window(tag="Recipe Calculator", width=300, height=300):
     dpg.add_text("Hello, world")
     dpg.add_button(label="Save")
     dpg.add_input_text(label="string", default_value="Quick brown fox")
@@ -31,5 +31,6 @@ with dpg.window(label="Recipe Calculator", width=300, height=300):
 dpg.create_viewport(title='Custom Title', width=600, height=200)
 dpg.setup_dearpygui()
 dpg.show_viewport()
+dpg.set_primary_window('Recipe Calculator', True)
 dpg.start_dearpygui()
 dpg.destroy_context()
--- test.py
+++ test.py
@@ -1,14 +1,31 @@
 import dearpygui.dearpygui as dpg
 
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=600, height=300)
 
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello, world")
-    dpg.add_button(label="Save")
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+# Callback to monitor changes
+def connection_callback(sender, app_data):
+    # Recreate default connection if it's removed
+    if app_data['link_id'] == "default_connection" and app_data['deleted']:
+        dpg.add_node_link("node1", "node2", parent="node_editor", id="default_connection")
 
+
+# Initialize node editor with default nodes and connections
+with dpg.window(label="Tutorial", width=400, height=400):
+    # Add nodes and pins, e.g., Node 1, Pin A connected to Node 2, Pin B
+    with dpg.node_editor(callback=connection_callback):
+        with dpg.node(label='node1'):
+            with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Output, label='node1', tag='node1'):
+                dpg.add_text("Output Pin A")
+        with dpg.node(label='node2'):
+            with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Input, label='node2', tag='node2'):
+                dpg.add_text("Input Pin B")
+
+        # Create a connection
+        dpg.add_node_link("node1", "node2", tag="default_connection")
+
+
+# Initialize Dear PyGui
+dpg.create_viewport(title='Custom Node Editor')
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
