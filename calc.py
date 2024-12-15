99ece2b5dd3acaf3626f45caea24bafb55d5e37a
June Simmons <junecsimmons@gmail.com>
2024-12-14 22:31:39 -0600

    Updated visuals, added Item class that contains a universal list of all items, each with a list of recipes.

--- calc.py
+++ calc.py
@@ -6,6 +6,8 @@ EXTRACTED_ITEMS = ('Iron Ore', 'Copper Ore', 'Caterium Ore', 'Limestone', 'Coal'
                     'Hatcher Remains', 'Spitter Remains', 'Stinger Remains', 'Wood', 'Leaves')
 
 class Recipe:
+    all_recipes = []
+
     def __init__(self, name, alt, ficsmas, building, ingredients, outputs):
         self.name = name
         self.building = building
@@ -15,35 +17,118 @@ class Recipe:
         self.items = [item['Item'] for item in outputs]
         self.ipm = [item['Per-minute'] for item in outputs]
         self.clockspeed = 1
+        self.is_base = True if self.ingredients is None else False
 
-    def __contains__(self, item):
-        if not isinstance(item, str):
+        Recipe.all_recipes.append(self)
+
+    def __contains__(self, _item):
+        if not isinstance(_item, str):
             raise TypeError("Item is not a string")
 
-        return item in self.items
+        return _item in self.items
 
     def calc_clockspeed(self):
         pass
 
     def recipes_for_inputs(self):
-        _ingredient_recipes = []
-        for _ingredient in self.ingredients:
-            if any(extracted_item in _ingredient for extracted_item in EXTRACTED_ITEMS):
-                continue
+        if not self.is_base:
+            _ingredient_recipes = []
+            for _ingredient in self.ingredients:
+                if any(extracted_item in _ingredient for extracted_item in EXTRACTED_ITEMS):
+                    continue
 
-            for _item in recipes:
-                if _ingredient['Item'] in _item and not _item.alt:
-                    _ingredient_recipes.append(_item)
+                for _item in Recipe.all_recipes:
+                    if _ingredient['Item'] in _item and not _item.alt:
+                        _ingredient_recipes.append(_item)
 
-        return _ingredient_recipes
+            return _ingredient_recipes
+        else:
+            raise TypeError("Item is a base with no prior ingredients.")
+
+
+class Item:
+
+    all_items = []
+
+    def __init__(self, name, recipe, add_to_list=True):
+        self.name = name
+        self.recipes = [recipe]
+
+        if add_to_list:
+            self.__add_to_list()
+
+    def __add_to_list(self):
+        """Intended for internal use only."""
+        # Check if an item with the same name already exists
+        for _item in Item.all_items:
+            if _item.name == self.name:
+                # If found, add recipe to the existing item
+                _item.recipes.extend(self.recipes)
+                break
+        else:
+            # If no matching item is found, add self to all_items
+            Item.all_items.append(self)
+
+
+    def append(self, recipe):
+        # Add a recipe to the list.
+        self.recipes.append(recipe)
+
+    def extend(self, iterable):
+        self.recipes.extend(iterable)
+
+    def pop(self, index=-1):
+        # Remove and return a recipe at the specified index.
+        return self.recipes.pop(index)
+
+    def __getitem__(self, index):
+        # Access a recipe by index.
+        return self.recipes[index]
+
+    def __setitem__(self, index, value):
+        # Set a recipe at a specific index.
+        self.recipes[index] = value
+
+    def __delitem__(self, index):
+        # Delete a recipe at a specific index.
+        del self.recipes[index]
+
+    def __contains__(self, recipe):
+        for contained_recipe in self.recipes:
+            if contained_recipe.name == recipe.name:
+                return True
+
+        else: return False
+
+    def __len__(self):
+        # Get the number of recipes.
+        return len(self.recipes)
+
+    def __iter__(self):
+        # Iterate over the recipes.
+        return iter(self.recipes)
+
+    def __repr__(self):
+        return f"Item(name={self.name}, recipes={self.recipes})"
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __eq__(self, other):
+        return self.name == other
+
 
 #problems: Compacted Coal, Encased Uranium Cell,
-recipes = []
 with open('recipes.json', 'r') as file:
     for _recipe in json.load(file):
-        recipes.append(Recipe(*_recipe.values()))
+        new_recipe = Recipe(*_recipe.values())
+        for item in new_recipe.items:
+            Item(item, new_recipe)
 
+print(vars(Recipe.all_recipes[28]))
+print(len(Recipe.all_recipes))
+print(list(vars(i) for i in Recipe.all_recipes[28].recipes_for_inputs()))
 
-print(vars(recipes[28]))
-print(len(recipes))
-print(list(vars(i) for i in recipes[28].recipes_for_inputs()))
+print(vars(Item.all_items[1]))
+print(len(Item.all_items))
+print(list(vars(i) for i in Item.all_items[1].recipes))
---
+++ gui.py
@@ -0,0 +1,109 @@
+import sys
+
+from PySide2.QtWidgets import (QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem,
+                               QGraphicsTextItem, QGraphicsLineItem, QGraphicsRectItem, QGraphicsPathItem, QComboBox,
+                               QGraphicsProxyWidget)
+from PySide2.QtGui import QPen, QBrush, QColor, QPainterPath
+from PySide2.QtCore import Qt, QRectF, QPointF
+
+class Node(QGraphicsRectItem):
+    def __init__(self, x, y, label="Node", width=250, height=100):
+        super().__init__(-width/2, -height/2, width, height)  # Create a rectangular node
+        self.setPos(x, y)
+        self.setBrush(QBrush(QColor(100, 150, 250)))  # Fill color
+        self.setPen(QPen(Qt.black, 2))  # Border color
+
+        # Add a label to the node
+        text = QGraphicsTextItem(label, self)
+        text.setDefaultTextColor(Qt.white)
+        text.setPos(-text.boundingRect().width() / 2, -text.boundingRect().height() / 2)  # Center label in node
+
+        combo_box = QComboBox()
+        combo_box.addItem("Option 1")
+        combo_box.addItem("Option 2")
+        combo_box.addItem("Option 3")
+        proxy_widget_combo = QGraphicsProxyWidget(self)
+        proxy_widget_combo.setWidget(combo_box)
+        proxy_widget_combo.setPos(10, 10)
+
+class FlowchartWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Flowchart Example with PySide2")
+        self.setGeometry(100, 100, 800, 600)
+
+        # Set up the scene and view
+        self.scene = QGraphicsScene()
+        self.view = QGraphicsView(self.scene, self)
+        self.setCentralWidget(self.view)
+
+        # Create nodes
+        node1 = Node(0, 200, "Start")
+        node2 = Node(300, 200, "Process")
+        node3 = Node(600, 100, "End")
+
+        # Add nodes to the scene
+        self.scene.addItem(node1)
+        self.scene.addItem(node2)
+        self.scene.addItem(node3)
+
+        # Connect nodes with lines
+        self.add_path_connection(node1, node2)
+        self.add_path_connection(node2, node3)
+
+    def add_path_connection(self, node1, node2):
+        # Add path object
+        _path = QPainterPath()
+
+        if abs(node1.pos().y() - node2.pos().y()) <= (node2.pos().x() - node2.boundingRect().width() / 2) - (node1.pos().x() + node1.boundingRect().width() / 2):
+            curve_radius = (node1.pos().y() - node2.pos().y()) / 2.75  # Hard coded appearance quality number
+
+        else:
+            curve_radius = ((node2.pos().x() - node2.boundingRect().width() / 2) - (node1.pos().x() + node1.boundingRect().width() / 2)) / 2.75
+
+        print(curve_radius)
+
+        # Start is midpoint of the right side of node 1, end is midpoint of left side of node 2
+        _start = QPointF(node1.pos().x() + node1.boundingRect().width() / 2, node1.pos().y())
+        _end = QPointF(node2.pos().x() - node2.boundingRect().width() / 2, node2.pos().y())
+        # Midpoint between nodes
+        x_midpoint = ((_end.x() - _start.x()) / 2) + _start.x()
+        # Control points offset by curve radius from midline
+        control_point1 = QPointF(x_midpoint + curve_radius, _start.y())
+        control_point2 = QPointF(x_midpoint - curve_radius, _end.y())
+        control_point3 = QPointF(x_midpoint - curve_radius, _start.y())
+        control_point4 = QPointF(x_midpoint + curve_radius, _end.y())
+
+        # Set path location
+        _path.moveTo(_start)
+
+        # Node1 beneath node2
+        if _start.y() >= _end.y():
+            _path.lineTo(control_point3)
+            _path.cubicTo(control_point1, control_point2, control_point4)
+            _path.lineTo(_end)
+
+        # Node1 above node2
+        else:
+            _path.lineTo(control_point1)
+            _path.cubicTo(control_point3, control_point4, control_point2)
+            _path.lineTo(_end)
+
+        # Add the path to the scene
+        path_item = QGraphicsPathItem(_path)
+        path_item.setPen(QPen(Qt.black, 2))
+        self.scene.addItem(path_item)
+
+    def add_point_to_scene(self, point, color):
+        # Visualize a point as a small circle
+        radius = 5
+        ellipse = QGraphicsEllipseItem(point.x() - radius, point.y() - radius, 2 * radius, 2 * radius)
+        ellipse.setBrush(QBrush(color))
+        ellipse.setPen(QPen(Qt.black))
+        self.scene.addItem(ellipse)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = FlowchartWindow()
+    window.show()
+    sys.exit(app.exec_())
